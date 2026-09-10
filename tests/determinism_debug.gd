2c5ff4804beed3ae6adb93ff138afaf27ffde308
eghosa001 <aighewieghosa111@gmail.com>
2026-09-10 11:21:19 +0100

    Fix deterministic diagnostic traversal

--- tests/determinism_debug.gd
+++ tests/determinism_debug.gd
@@ -7,11 +7,14 @@ func _init() -> void:
 	var generator = WorldGeneratorClass.new()
 	var a: Dictionary = generator.create_world(12345)
 	var b: Dictionary = generator.create_world(12345)
-	print("[DETERMINISM DEBUG] world: %s" % _first_difference(a, b))
+	var world_diff := _first_difference(a, b)
+	print("[DETERMINISM DEBUG] world: %s" % ("<identical>" if world_diff == "" else world_diff))
 	var engine = MatchEngineClass.new()
 	var first: Dictionary = engine.simulate_match(a.clubs[0], a.clubs[1], a.players, 827183927)
 	var second: Dictionary = engine.simulate_match(a.clubs[0], a.clubs[1], a.players, 827183927)
-	print("[DETERMINISM DEBUG] match: %s" % _first_difference(first, second))
+	for key in ["lineups", "events", "substitutions", "stats", "ratings", "home_goals", "away_goals"]:
+		var diff := _first_difference(first[key], second[key], "root.%s" % key)
+		print("[DETERMINISM DEBUG] match %s: %s" % [key, "<identical>" if diff == "" else diff])
 	quit(0)
 
 func _first_difference(left: Variant, right: Variant, path: String = "root") -> String:
@@ -27,7 +30,7 @@ func _first_difference(left: Variant, right: Variant, path: String = "root") -> 
 			var diff: String = _first_difference(left[key], right[key], "%s.%s" % [path, str(key)])
 			if diff != "":
 				return diff
-		return "<identical>"
+		return ""
 	if left_type == TYPE_ARRAY:
 		if left.size() != right.size():
 			return "%s array size %d != %d" % [path, left.size(), right.size()]
@@ -35,7 +38,7 @@ func _first_difference(left: Variant, right: Variant, path: String = "root") -> 
 			var diff: String = _first_difference(left[i], right[i], "%s[%d]" % [path, i])
 			if diff != "":
 				return diff
-		return "<identical>"
+		return ""
 	if left != right:
 		return "%s value %s != %s" % [path, str(left), str(right)]
 	return ""
