722915591791247ce4e162b276c65191802d52ce
AIGHEWI EGHOSA <aighewieghosa111@gmail.com>
2026-09-13 15:57:04 +0100

    Validate live match session and committer directly

--- tests/managed_matchday_compile_test.gd
+++ tests/managed_matchday_compile_test.gd
@@ -4,9 +4,8 @@ const Session = preload("res://simulation/match/managed_match_session.gd")
 
 func _init() -> void:
 	assert(Session.new() != null)
-	var coordinator_script = load("res://application/career/managed_matchday_coordinator.gd")
-	assert(coordinator_script != null)
-	var coordinator = coordinator_script.new()
-	assert(coordinator != null)
+	var committer_script = load("res://application/career/managed_matchday_committer.gd")
+	assert(committer_script != null)
+	assert(committer_script.new() != null)
 	print("[TEST] MANAGED MATCHDAY COMPILE PASS")
 	quit(0)
