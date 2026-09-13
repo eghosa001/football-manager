97772f87e027692aafa7a9dadfbed01d6a99a725
AIGHEWI EGHOSA <aighewieghosa111@gmail.com>
2026-09-13 15:52:09 +0100

    Diagnose managed matchday with runtime load

--- tests/managed_matchday_compile_test.gd
+++ tests/managed_matchday_compile_test.gd
@@ -1,10 +1,12 @@
 extends SceneTree
 
 const Session = preload("res://simulation/match/managed_match_session.gd")
-const Coordinator = preload("res://application/career/managed_matchday_coordinator.gd")
 
 func _init() -> void:
 	assert(Session.new() != null)
-	assert(Coordinator.new() != null)
+	var coordinator_script = load("res://application/career/managed_matchday_coordinator.gd")
+	assert(coordinator_script != null)
+	var coordinator = coordinator_script.new()
+	assert(coordinator != null)
 	print("[TEST] MANAGED MATCHDAY COMPILE PASS")
 	quit(0)
