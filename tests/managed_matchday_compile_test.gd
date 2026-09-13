6260fd7f3a0de84c53248abecd2122af483e257f
AIGHEWI EGHOSA <aighewieghosa111@gmail.com>
2026-09-13 19:09:24 +0100

    Cover managed matchday coordinator compilation

--- tests/managed_matchday_compile_test.gd
+++ tests/managed_matchday_compile_test.gd
@@ -4,6 +4,9 @@ const Session = preload("res://simulation/match/managed_match_session.gd")
 
 func _init() -> void:
 	assert(Session.new() != null)
+	var coordinator_script = load("res://application/career/managed_matchday_coordinator.gd")
+	assert(coordinator_script != null)
+	assert(coordinator_script.new() != null)
 	var committer_script = load("res://application/career/managed_matchday_committer.gd")
 	assert(committer_script != null)
 	assert(committer_script.new() != null)
